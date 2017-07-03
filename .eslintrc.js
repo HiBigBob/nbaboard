723d1e707c142f2e1a349247feff779a9e88644f
Adrien Bouttier <hibigbob@gmail.com>
2017-07-03 23:26:20 +0200

    Add new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,8 @@ module.exports = {
     'import/extensions': 0,
     'import/newline-after-import': 0,
     'no-multi-assign': 0,
+    'quote-props': 0,
+    'no-console': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
