8cd6e88e7e5ae352a6784861d475daccc2e9f11e
Adrien Bouttier <hibigbob@gmail.com>
2017-07-14 15:08:35 +0200

    delete commenter

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -10,10 +10,9 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
   components: { App },
-  router,
   store,
+  router,
   template: '<App/>',
 }).$mount('#app');
