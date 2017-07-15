e2c0bf20675c1ac343b5a3da6c3f4f81e07fb914
Adrien Bouttier <hibigbob@gmail.com>
2017-07-15 22:05:49 +0200

    Import vuex structure

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,11 +1,20 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
-import modules from './modules';
+import * as actions from './actions';
+import * as getters from './getters';
+import cart from './modules/cart';
+import products from './modules/products';
 
 Vue.use(Vuex);
 
+const debug = process.env.NODE_ENV !== 'production';
+
 export default new Vuex.Store({
-  modules,
-  strict: process.env.NODE_ENV !== 'production',
+  actions,
+  getters,
+  modules: {
+    cart,
+    products,
+  },
+  strict: debug,
 });
