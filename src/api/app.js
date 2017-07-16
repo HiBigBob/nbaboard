67daae4254ca5a98dde0695f779148df9635724f
Adrien Bouttier <hibigbob@gmail.com>
2017-07-16 16:26:01 +0200

    Refacto with method reqwest

--- src/api/app.js
+++ src/api/app.js
@@ -11,33 +11,17 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  fetch('http://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate=04/09/2017', {
-    method: 'GET',
-    headers: {
-      'Referer': 'http://stats.nba.com/',
-      'DNT': '1',
-      'Accept-Encoding': 'gzip, deflate, sdch',
-      'Accept-Language': 'en',
-      'Accept': '*/*',
-      'Connection': 'keep-alive',
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-    },
-  }).then(res => res.json()).then(json => res.json(transformer(json.resultSets[0]))).catch(json => res.json(json));
+  const url = 'http://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate=04/09/2017';
+  reqwest(url).then((response) => {
+    res.json(transformer(response.resultSets[0]));
+  });
 });
 
 app.get('/leaguedashplayerstats', (req, res) => {
-  fetch('http://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2016-17&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=', {
-    method: 'GET',
-    headers: {
-      'Referer': 'http://stats.nba.com/',
-      'DNT': '1',
-      'Accept-Encoding': 'gzip, deflate, sdch',
-      'Accept-Language': 'en',
-      'Accept': '*/*',
-      'Connection': 'keep-alive',
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-    },
-  }).then(res => res.json()).then(json => res.json(transformer(json.resultSets[0]))).catch(json => res.json(json));
+  const url = 'http://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2016-17&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=';
+  reqwest(url).then((response) => {
+    res.json(transformer(response.resultSets[0]));
+  });
 });
 
 const transformer = (result) => {
@@ -52,6 +36,21 @@ const transformer = (result) => {
       return Object.assign({}, item);
     }),
   };
+};
+
+const reqwest = (url) => {
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      'Referer': 'http://stats.nba.com/',
+      'DNT': '1',
+      'Accept-Encoding': 'gzip, deflate, sdch',
+      'Accept-Language': 'en',
+      'Accept': '*/*',
+      'Connection': 'keep-alive',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    },
+  }).then(res => res.json()).catch(res => console.log(res));
 };
 
 app.listen(9081, () => {
