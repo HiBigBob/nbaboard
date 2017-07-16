3d54d3109b7aff3578fe4897a4832917914675b8
Adrien Bouttier <hibigbob@gmail.com>
2017-07-16 15:38:03 +0200

    Add temporary function to transform result

--- src/api/app.js
+++ src/api/app.js
@@ -22,8 +22,37 @@ app.get('/', (req, res) => {
       'Connection': 'keep-alive',
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
     },
-  }).then(res => res.json()).then(json => res.json(json)).catch(json => res.json(json));
+  }).then(res => res.json()).then(json => res.json(transformer(json.resultSets[0]))).catch(json => res.json(json));
 });
+
+app.get('/leaguedashplayerstats', (req, res) => {
+  fetch('http://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2016-17&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight=', {
+    method: 'GET',
+    headers: {
+      'Referer': 'http://stats.nba.com/',
+      'DNT': '1',
+      'Accept-Encoding': 'gzip, deflate, sdch',
+      'Accept-Language': 'en',
+      'Accept': '*/*',
+      'Connection': 'keep-alive',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    },
+  }).then(res => res.json()).then(json => res.json(transformer(json.resultSets[0]))).catch(json => res.json(json));
+});
+
+const transformer = (result) => {
+  return {
+    headers: result.headers.map((item, index) => {
+      return {
+        title: item,
+        key: index,
+      };
+    }),
+    values: result.rowSet.slice(0, 50).map((item) => {
+      return Object.assign({}, item);
+    }),
+  };
+};
 
 app.listen(9081, () => {
   console.log('Express server listening on %d', 9081);
