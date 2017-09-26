2c9c23357c804d2c6ebb0b4fcb88e2d71fc5095a
Adrien Bouttier <hibigbob@gmail.com>
2017-09-26 13:50:11 +0200

    Change to promise

--- src/renderer/api/stats.js
+++ src/renderer/api/stats.js
@@ -1,15 +1,18 @@
 import Vue from 'vue';
 
 export default {
-  getDashPlayer(cb) {
-    Vue.http.get('/player', {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      }
-    }).then((response) => {
-      cb(response.data);
-    })
-    .catch(() => {
+  getDashPlayer() {
+    return new Promise((resolve, reject) => {
+      Vue.http.get('/player', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        }
+      }).then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
     });
   },
 };
